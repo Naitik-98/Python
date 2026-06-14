f5494ca97b580cf382b96e9107048f8c35d834df
Naitik <134581392+Naitik-98@users.noreply.github.com>
2026-06-14 11:51:43 +0600

    Add files via upload

---
+++ Lab01/problem2.py
@@ -0,0 +1,7 @@
+
+num01 = input("Enter num01 :")
+num02 = input("Enter num02 :")
+print("Number 01 : ", num01)
+print("Number 02 : ", num02)
+
+print(num01 + num02)
---
+++ Lab02/problem1.py
@@ -0,0 +1,10 @@
+num01 = input("Enter num01 :")
+num02 = input("Enter num02 :")
+
+print("before swap")
+print("Number 01 : ", num01)
+print("Number 02 : ", num02)
+
+print("after swap")
+print("Number 01 : ", num02)
+print("Number 02 : ", num01)
