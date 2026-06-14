e664a162b1a88af76134c4d4273ed3844270116b
Naitik <134581392+Naitik-98@users.noreply.github.com>
2026-06-14 12:27:10 +0600

    Update problem4.py

--- Lab03/problem4.py
+++ Lab03/problem4.py
@@ -1,14 +1,1 @@
-sum_primes = 0
 
-for num in range(2, 1000):
-    is_prime = True
-
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            is_prime = False
-            break
-
-    if is_prime:
-        sum_primes += num
-
-print("Sum of primes below 1000 =", sum_primes)
